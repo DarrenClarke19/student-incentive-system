ec30647a3932dd520cf742438c7c13621cd2531d
Darren Clarke <darrenjustinclarke@yahoo.com>
2025-09-26 08:24:23 -0400

    tests failing

--- App/tests/test_app.py
+++ App/tests/test_app.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from App.main import create_app
 from App.database import db, create_db
-from App.models import User
+from App.models import User, UserRoleEnum
 from App.controllers import (
     create_user,
     get_all_users_json,
@@ -22,24 +22,24 @@ LOGGER = logging.getLogger(__name__)
 class UserUnitTests(unittest.TestCase):
 
     def test_new_user(self):
-        user = User("bob", "bobpass")
+        user = User("bob", "bobpass", role=UserRoleEnum.STUDENT)
         assert user.username == "bob"
 
     # pure function no side effects or integrations called
     def test_get_json(self):
-        user = User("bob", "bobpass")
+        user = User("bob", "bobpass", role=UserRoleEnum.STUDENT)
         user_json = user.get_json()
-        self.assertDictEqual(user_json, {"id":None, "username":"bob"})
+        self.assertDictEqual(user_json, {"id":None, "username":"bob", "role":"student"})
     
     def test_hashed_password(self):
         password = "mypass"
-        hashed = generate_password_hash(password, method='sha256')
-        user = User("bob", password)
+        hashed = generate_password_hash(password)
+        user = User("bob", password, role=UserRoleEnum.STUDENT)
         assert user.password != password
 
     def test_check_password(self):
         password = "mypass"
-        user = User("bob", password)
+        user = User("bob", password, role=UserRoleEnum.STUDENT)
         assert user.check_password(password)
 
 '''
@@ -57,18 +57,18 @@ def empty_db():
 
 
 def test_authenticate():
-    user = create_user("bob", "bobpass")
+    user = create_user("bob", "bobpass", role=UserRoleEnum.STUDENT)
     assert login("bob", "bobpass") != None
 
 class UsersIntegrationTests(unittest.TestCase):
 
     def test_create_user(self):
-        user = create_user("rick", "bobpass")
+        user = create_user("rick", "bobpass", role=UserRoleEnum.STAFF)
         assert user.username == "rick"
 
     def test_get_all_users_json(self):
         users_json = get_all_users_json()
-        self.assertListEqual([{"id":1, "username":"bob"}, {"id":2, "username":"rick"}], users_json)
+        self.assertListEqual([{"id":1, "username":"bob", "role": "student"}, {"id":2, "username":"rick", "role":"staff" }], users_json)
 
     # Tests data changes in the database
     def test_update_user(self):
